c5d865c651ad800e120310d33761b21ef47014f3
Rohan Sai <rohansai231098@gmail.com>
2020-08-05 22:03:02 -0700

    Updates

---
+++ precache-manifest.77dc4b05a6a210b878eaada889b3123a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4760e53e2deec324b82533c126eee150",
+    "url": "/QuickList/index.html"
+  },
+  {
+    "revision": "aebb0938b0f90363c3ab",
+    "url": "/QuickList/static/css/main.97858622.chunk.css"
+  },
+  {
+    "revision": "bbe81b7052d923805b0d",
+    "url": "/QuickList/static/js/2.71386d58.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/QuickList/static/js/2.71386d58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aebb0938b0f90363c3ab",
+    "url": "/QuickList/static/js/main.c61425ad.chunk.js"
+  },
+  {
+    "revision": "a2c55bb3f6009530998d",
+    "url": "/QuickList/static/js/runtime-main.69dc81f3.js"
+  }
+]);
